bdcccef31fdab696b211d6be5baa5b232364e2fb
Emissions <unitedrealm@hotmail.com>
2019-05-12 14:11:37 +0100

    Cleared the original workshop files in favour for updated ones

--- commands/eval.js
+++
@@ -1,3 +0,0 @@
-exports.run = (client, message, args) => {
-    client.functions.generalFunctions.evaluate(message, args);
-}
--- events/guildMemberAdd.js
+++
@@ -1,5 +0,0 @@
-module.exports = (client, member) => {
-    // create new database for user ID
-    let tempCh = member.guild.channels.find(x => x.name === "master_chat");
-    tempCh.send(`<{:botcommand:}>newprocess ${member.id}`);
-}
